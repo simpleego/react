f35a1ac13183d0bf68d79a145d6ea820192139d2
Jong Cheon Park <simpleego@gmail.com>
2025-10-24 16:03:08 +0900

    Add files via upload

---
+++ ReactStudy/src/chapter_06_1/Total.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+
+function Total(){
+
+    const [values, setValues] = useState({a:0, b:0, c:0});
+
+    const handleChange = (e)=>{
+        const {name, value} = e.target;
+        setValues((prev)=>({
+            ...prev,
+            [name]: Number(value),
+        }));
+    };
+
+    // 총점 처리
+    const total = values.a + values.b + values.c;  
+    
+    // 평균 처리
+
+
+    return(
+
+        <div>
+            <h2>합계 계산기</h2>
+            <div>
+                <label htmlFor="">국어 :</label>
+                <input type="number" name="a" value={values.a} onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="">영어 :</label>
+                <input type="number" name="b" value={values.b} onChange={handleChange}/>
+            </div>
+            <div>
+                <label htmlFor="">수학 :</label>
+                <input type="number" name="c" value={values.c} onChange={handleChange}/>
+            </div>
+            <h3>총합 : {total} </h3>
+            <h3>평균 : {avg} </h3>
+            <h3>학점 : {grade} </h3>
+        </div>
+
+    )
+}
+
+export default Total;
